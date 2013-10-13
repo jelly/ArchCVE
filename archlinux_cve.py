4b7fbc6c9a722e3d89f537955710bbffe12e0fb8
Jelle van der Waa <jelle@vdwaa.nl>
2013-10-13 16:27:40 +0200

    redo outputting information

--- archlinux_cve.py
+++ archlinux_cve.py
@@ -3,37 +3,46 @@ from lxml import etree
 import pyalpm
 from pycman import config
 
+# Setup pyalpm stuff
 repos = ['core','community','extra','testing','multilib','multilib-testing','community-testing']
 configpath  = '/etc/pacman.conf'
 handle = False
 handle = config.init_with_config(configpath)
+syncdbs = handle.get_syncdbs()
 
+# search helper 
 def search(package):
     results = []
-    for db in handle.get_syncdbs():
+    for db in syncdbs:
             results += db.search(package)
     return results
 
 
 #url = 'http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-modified.xml'
 url = 'nvdcve-2.0-modified.xml'
+
+# setup parser for XML file
 parser = etree.XMLParser(ns_clean=True, recover=True)
 try:
     xml = etree.parse(url,parser)
 except ValueError:
     print ("Couldn't parse source {0} ".format(url))
 
+# Setup XML namespaces
 ns = {'vuln': 'http://scap.nist.gov/schema/vulnerability/0.4',
       'cvss': 'http://scap.nist.gov/schema/cvss-v2/0.2'}
-      #xmlns:scap-core="http://scap.nist.gov/schema/scap-core/0.1" xmlns="http://scap.nist.gov/schema/feed/vulnerability/2.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:patch="http://scap.nist.gov/schema/patch/0.1" xmlns:cpe-lang="http://cpe.mitre.org/language/2.0" nvd_xml_version="2.0" pub_date="2013-09-22T01:03:08" xsi:schemaLocation="http://scap.nist.gov/schema/patch/0.1 http://nvd.nist.gov/schema/patch_0.1.xsd http://scap.nist.gov/schema/scap-core/0.1 http://nvd.nist.gov/schema/scap-core_0.1.xsd http://scap.nist.gov/schema/feed/vulnerability/2.0 http://nvd.nist.gov/schema/nvd-cve-feed_2.0.xsd">
+
 
 root = xml.getroot()
 found = False
+
+# Loop over CVE's
 for entry in root:
     summary = entry.find('vuln:summary',ns)
     products = entry.find('vuln:vulnerable-software-list',ns)
     #print ("Parsing CVE id: {}".format(entry.attrib['id']))
     #print(summary.text)
+    affectedpkgs = ""
     if products is not None:
         for product in products.iterchildren():
 
@@ -49,16 +58,23 @@ for entry in root:
             for pkg in results:
                 ver = pkg.version.split('-')[0]
                 if ver == version:
-                    print(summary.text)
-                    print("%s/%s %s CVE: program %s CVE version: %s" % (pkg.db.name, pkg.name, pkg.version,program,version))
+                    affectedpkgs += "{}/{} {} - CVE program: {} version: {}\n".format(pkg.db.name, pkg.name, pkg.version, program, version)
                     found = True
-
+            
                     
 
     if found:
+        print ("\033[1;31mCVE id: http://cve.mitre.org/cgi-bin/cvename.cgi?name={}\033[1;m".format(entry.attrib['id']))
+        print ("Possible affected programs")
+        print (affectedpkgs) 
+        print("\033[1;31mSUMMARY:\033[1;m")
+        print(summary.text)
+
+        print ("\033[1;31mREFERENCES:\033[1;m")
+        # Print related references
         references = entry.findall('vuln:references',ns)
         for reference in references:
             ref = reference.find('vuln:reference',ns)
-            print("reference {}".format(ref.text))
+            print(ref.attrib['href'])
         found = False
-        print ("#####################################################################################################")
+        print ("\n")
